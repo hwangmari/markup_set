6a11521fb9e6b2276cf9c8d9ea3dbe4c743eaeac
hwangmari <hwangmari@naver.com>
2020-10-29 21:50:09 +0900

    스벨트 과제 가위바위보 만들기

--- Svelte/my-svelte-project/src/App.svelte
+++ Svelte/my-svelte-project/src/App.svelte
@@ -3,33 +3,31 @@
 	let computer = "";
 	let print = "";
 	let my = "";
+	let scoops = "";
 
-	function ACTION() {
+	function ACTION () {
 		num = parseInt (Math.random() * 3)
 		if (num === 0 ) computer = "가위"
 		else if ( num === 1 ) computer = "바위"
 		else if ( num === 2 ) computer = "보"	
 		
 	}
-
-
+	let timer = setInterval (ACTION, 120);
 
 	function SEND (my) {
 		clearInterval(timer)
 		
 		if ( computer === my ) print = "비겼다 ~ㅅ ~" 
-		else if ( computer === "가위" && my === "바위" ) print = "이겼다 +ㅅ +"
-		else if ( computer === "가위" && my === "보" ) print = "졌다ㅜㅜ"
+		else if ( computer === "가위" && my === "바위" ) print = "이겼다 +ㅅ +" 
+		else if ( computer === "가위" && my === "보" ) print = "졌다 ㅜ ㅜ"
 
 		else if ( computer === "바위" && my === "가위" ) print = "이겼다 +ㅅ +"
-		else if ( computer === "바위" && my === "보" ) print = "졌다ㅜㅜ"
+		else if ( computer === "바위" && my === "보" ) print = "졌다 ㅜ ㅜ"
 
 		else if ( computer === "보" && my === "가위" ) print = "이겼다 +ㅅ +"
-		else if ( computer === "보" && my === "바위" ) print = "졌다ㅜㅜ"
+		else if ( computer === "보" && my === "바위" ) print = "졌다 ㅜ ㅜ"
 		
 	}
-
-	let timer = setInterval(ACTION, 120);
 
 	// function RESET() {
 	// 	let timer = setInterval(ACTION, 300);
@@ -40,29 +38,35 @@
 	// 	if ( RESET ) let timer = setInterval(ACTION, 300);
 	// 	else if ( SEND )  clearInterval(timer) 
 	// }
-
+	// <button on:click={()} bind:group={scoops} value={1}>가위</button>
 </script>
 
 <main>
 	<h1>랜덤 가위바위보<br>
-		{computer}
+		<strong>{computer}</strong>
 	</h1>
 	<h2>
-		이겼다 비겼다 졌다 <br>
-		{print}
+		결과 <br>
+		<strong>{print}</strong>
 	</h2>
-	<h3>나의 결과값 {my}</h3>
+	<h3>승부다! </h3>
 
-	<button on:click={()=> SEND('가위')}>가위</button>
+	
+	<br>
+	<button on:click={() => SEND('가위')}>가위</button>
 	<button on:click={() => SEND('바위')}>바위</button>
 	<button on:click={() => SEND('보')}>보</button>
-
+	<br><br>
 	<button on:click={() => RESET('reset')}>
-		reset
+		다시하기
 	</button>
 </main>
 
 <style>
+	.active {
+		background-color: #ff3e00;
+		color: white;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
@@ -70,17 +74,22 @@
 		margin: 0 auto;
 	}
 
-	h1 {
+	h1 strong{
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
-
+	h2 strong{
+		display: block;
+		height:40px;
+		font-size:30px;
+		color:blue;
+	}
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
 	}
-	button{display:inline-block;width:100px;height:50px;border:1px solid #000;background:#fff;}
+	button{display:inline-block;width:80px;height:50px;border:1px solid #000;background:#fff;}
 </style>
